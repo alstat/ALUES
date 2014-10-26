0695c21cc354a9e5a01b7a6d1a8ae72d03472a5e
Al-Ahmadgaid Asaad <alstated@gmail.com>
2014-10-26 22:44:43 +0800

    Modified

--- src/case_a.cpp
+++ src/case_a.cpp
@@ -5,8 +5,7 @@ using namespace Rcpp;
 // Below is a simple example of exporting a C++ function to R. You can
 // source this function into an R session using the Rcpp::sourceCpp 
 // function (or via the Source button on the editor toolbar)
-  
-  // For more on using Rcpp click the Help button on the editor toolbar
+// For more on using Rcpp click the Help button on the editor toolbar
 
 // [[Rcpp::export]]
 List case_a(NumericMatrix df, NumericMatrix score, CharacterMatrix suiClass, double Min, double Max, double mfNum,
